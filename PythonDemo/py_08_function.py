5991afaddf11493846af755e3bfbb58a76dd1103
Smilling <503950890@qq.com>
2025-07-04 22:01:18 +0800

    7月4日晚上在家提交内容

--- PythonDemo/py_08_function.py
+++ PythonDemo/py_08_function.py
@@ -62,6 +62,57 @@ print(comper(10,8))
 #内置函数
 import builtins
 print(dir(builtins))            #显示所有内置函数及变量，大写字母开头一般是内置常名，小写开头一般是内置函数名
+#abs:绝对值  sum：求和，必须是可迭代对象   min/max:求最小/大值
+# zip：j将可迭代对象作为参数，将对象中对应的元素打包成一个元组
+li = [1,2,3]
+li1 = ['a','b','c','d']
+data = zip(li,li1)
+print(data,'zip返回的数据类型是：',type(data))
+for i in zip(li,li1):   #方式一：for循环取出其中的元素，如果元素不一致，就按照长度最短的返回
+    print(i)
+
+li2 = [1,2,3]
+li3 = ['a','b','c','d']
+print(list(zip(li,li1)))            #转换为列表输出，参数都必须是可迭代元素
 
 
+#map：可以对可迭代对象中的没一个元素进行映射，分别去执行,map(func,item)，item中的每一个元素都会执行一次func函数
+li4 = [1,2,3]
+def func(a):
+    return a*a
+ma = map(func,li4)          #只需要写函数名，不需要括号，执行后返回每一个结果
+for i in ma:
+    print(i)
+
+#reduce():先按顺序把对象中的元素取出来两个，进行计算然后保存，在与第三个值进行计算（累加）   需要先导包
+#reduce(function,sequenccee)   function—函数必须有两个参数的函数（只能是两个），sequence—序列，必须是可迭代对象
+from functools import reduce
+li5 = [1,2,3,4]
+def add(a,b):
+    return a+b
+res = reduce(add,li5)
+print(res)
+
+
+#拆包  含义：对于函数中的多个返回数据，去掉元组，列表或者字典，直接获取里面数据的过程
+tua = (1,2,3,4,)
+print(tua)
+#单个取出元组中的元素
+#方式一：
+a,b,c,d = tua
+print('a=',a,'b=',b ,'c=',c,'d =',d )       #要求变量的个数和元组内元素的个数相同，不然报错，一般在获取元组值的时候使用
+#方式二：
+a,*b = tua                  #一般在函数调用时使用
+print(a,b)
+def funa(a,b,*arges):
+    print(a,b)
+    print(arges)
+funa(1,2,3,4,5,6,7,8,9)
+arg = (1,2,3,4,5,6,7,8,9)
+funa(*arg)
+
+def func(a,*b):
+    print(type(b))
+arg=(1,2,3,4)
+func(1,2,3,4)
 
