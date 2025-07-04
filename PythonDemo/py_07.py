4d08a6d944a757df5538ee4eb0746bd130319209
wode <503950890@qq.com>
2025-07-04 18:00:07 +0800

    7月4日公司学习内容提交

--- PythonDemo/py_07.py
+++ PythonDemo/py_07.py
@@ -1,4 +1,4 @@
-#python中的类型转换，忽略简单内容
+#python中的类型转换，忽略简单内容（（int），（float），（char）....）
 #eval()函数,执行一个字符窜表达式，并返回结果，eval（）可以实现字符串到list，dict，tuple，str之间的转换
 print(eval('10+10'))
 str = '[[1,2],[3,4],[5,6]]'             #字符串转换list
@@ -18,3 +18,43 @@ print(list('abcdefg'),type(list('abcdefg')))
 print(list((1,2,3,4)),type(list((1,2,3))))
 print(list({'米呦':'我的love','年龄':18}),type(list({'米呦':'我的love','年龄':18})))     #字典转换成列表会取键名作为列表的值
 
+#python中赋值操作完全共享资源，一个改变另一个也会跟这个改变，类似于c中的指针
+
+#浅拷贝<数据半共享>：会创建一个新的对象，拷贝第一层的数据，嵌套层（内部元素）会指向原来的地址
+#[1,2,3,4,5,[66,99,88]]（外层的地址不同，内层的地址相同）
+#优点：拷贝速度快，占用空间少，拷贝效率高
+import copy       #导入copy模块
+li = [1,2,3,4,5,[66,99,88]]
+li2 = copy.copy(li)
+print(li,id(li))            #id()产看内存地址
+print(li2,id(li2))
+li.append(8)
+print(li,id(li))
+print(li2,id(li2))
+#往嵌套列表添加元素
+li[5].append(55)
+print(li,id(li))
+print(li2,id(li2))
+print('li[5]的地址：',id(li[1]))                 #内层嵌套（内部元素）的地址相同
+print('li2[5]的地址：',id(li2[1]))
+
+#深拷贝<数据完全不共享>：第一层的数据和内部嵌套（内部元素）的其它层数据完全拷贝，拷贝后和原来的对象无任何关联
+#[1,2,3,4,5,[66,99,88]]（外层内层的地址都不同）
+#优点：拷贝效率高，节省内存
+import copy       #导入copy模块
+li1 = [1,2,3,4,5,[66,99,88]]
+li3 = copy.deepcopy(li1)
+print(li1,id(li1))            #id()产看内存地址
+print(li3,id(li3))
+print('li1[5]的地址：',id(li1[5]))                 #内层嵌套（内部元素）的地址也不相同
+print('li3[5]的地址：',id(li3[5]))
+li1.append(9)
+li1[5].append(55)
+print('li1:',li1)
+print('li3:',li3)
+
+#可变对象（内存地址不会变）：允许修改的对象就是可变对象，这种数据类型就是可变类型，常见可变类型：list、dict、set....
+set = {'1','2','3','4'}
+print(set)
+
+#不可变对象：如果修改对象的值就会分配新的内存空间，常见类型：int、char、float、tuple、str
---
+++ PythonDemo/py_08_function.py
@@ -0,0 +1,67 @@
+#python中函数的定义
+#格式：            def 函数名()：
+#                           函数体
+def login():
+    print("这是登录函数")
+login()
+
+#返回值
+def buy():
+    return '小米呦','月月鸟'          #返回多个值时，以元组的形式返回
+print(buy())
+
+#可变参数作为函数的参数：传入的值的数量可以是改变的，可以传入多个，也可以不传   格式 def func(*arge),传入多个参数时以元组的形式接收
+def func(*arge):
+    print(arge)
+    print(type(arge))
+func('小米呦','月月鸟')
+
+#关键字参数：格式    def func(**kwarges)以字典的形式接受参数
+def fun(**kwarges):
+    print(kwarges)
+    print(type(kwarges))            #字典类型
+fun(name = '小米呦',age = 18)          #采用键值的形式传值
+
+#变量的作用域
+#Python中想要在函数体内声明全局变量使用global关键字  global 变量名 ,global也可对以声明的全局变量进行修改（在函数体内修改）
+a = 111
+def func1():
+    global s
+    s = 999
+func1()
+print('输出函数内定义的全局变量s：',s)
+
+#nonlocal关键字    用来声明外层的局部变量，在外部函数先进行声明，内部函数使用nonlocal进行声明
+a = 10   #全局变量
+def outer():              #外函数
+    a = 5
+    def inner():           #内函数
+        nonlocal a              #nonloacl只能对上一层函数内的变量进行修改
+        a = 20
+        print('inner函数中a的值：',a)
+    inner()
+    print('outer函数中a的值：',a)
+outer()
+print('全局变量a的值:',a)
+
+#匿名函数 基本语法 ：函数名 = lambda 形参：返回值（表达式），lambda不需要写return来返回值，表达式本身就是返回
+#调用：结果 = 函数名（实参）
+def add(a,b):
+    return a+b
+print(add(1,3))
+
+add = lambda a,b,c:a+b+c            #匿名函数
+print(add(1,3,5))
+
+#lambda的应用
+#lambda结合if判断
+#特点lambda只能实现简单的的逻辑，如果逻辑复杂且代码量比较大不建议实用lambda降低代码的可读性
+comper  = lambda a,b:'a大于b' if a>b else 'a小于等于b'
+print(comper(10,8))
+
+#内置函数
+import builtins
+print(dir(builtins))            #显示所有内置函数及变量，大写字母开头一般是内置常名，小写开头一般是内置函数名
+
+
+
